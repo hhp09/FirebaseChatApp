ba7ed252de74a6942e080c3190f8bba1cb20a5d7
Hritik <hritik81298@gmail.com>
2018-09-10 23:36:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/hriti/android/model/User.java
@@ -0,0 +1,21 @@
+package com.example.hriti.android.model;
+
+    public class User {
+
+        public String username;
+        public String firstName;
+        public String lastName;
+
+        public User() {
+            // Default constructor required for calls to DataSnapshot.getValue(User.class)
+        }
+
+        public User(String username, String firstName, String lastName) {
+            this.username = username;
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+    }
+
+
